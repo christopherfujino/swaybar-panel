3655d9942acb818d57e47956651644c1bd676e58
Christopher Fujino <christopherfujino@gmail.com>
2026-03-30 11:21:29 -0700

    add date

--- battery.go
+++ battery.go
@@ -9,23 +9,32 @@ import (
 
 const PREFIX = "/sys/class/power_supply/qcom-battmgr-bat/energy_"
 
-func batteryPart(part string) float64 {
+func batteryPart(part string) (float64, error) {
 	diskPath := fmt.Sprintf("%s%s", PREFIX, part)
 	bytes, err := os.ReadFile(diskPath)
 	if err != nil {
-		return -1; // TODO
+		return 0, err;
 	}
 	floatValue, err := strconv.ParseFloat(strings.TrimSpace(string(bytes)), 64)
 	if err != nil {
-		return -1; // TODO
+		return 0, err;
 	}
 
-	return floatValue
+	return floatValue, nil
 }
 
 func batteryPercent() string {
-	full := batteryPart("full")
-	current := batteryPart("now")
-	empty := batteryPart("empty")
+	full, err := batteryPart("full")
+	if err != nil {
+		panic(err)
+	}
+	current, err := batteryPart("now")
+	if err != nil {
+		panic(err)
+	}
+	empty, err := batteryPart("empty")
+	if err != nil {
+		panic(err)
+	}
 	return fmt.Sprintf("%.1f%%", 100*(current-empty)/(full-empty))
 }
--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func main() {
 	fmt.Printf("{\"version\":1}\n[")
 	for {
 		now := time.Now()
-		fmt.Printf("[{\"full_text\": \"%s\"},{\"full_text\": \"B(%s)\"}],", now.Format("15:04"), batteryPercent())
+		fmt.Printf("[{\"full_text\": \"%s\"}, {\"full_text\": \"%s\"},{\"full_text\": \"B(%s)\"}],", now.Format("Jan 02"), now.Format("15:04"), batteryPercent())
 		time.Sleep(time.Second * 5)
 	}
 }
